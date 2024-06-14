74571eb1f2366caa79b3c0932f725af0eb3a3a41
srinivaschowdary969 <mechden143r@gmail.com>
2024-06-14 02:39:48 -0700

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="srinu",
+    author_email="srinivas.bogineni13gmail.com",
+    packages=find_packages()
+)
